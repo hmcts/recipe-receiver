46c9a65cb68bc0a7bbd2555492d678960cefca9f
msl8r <matt.slater@hmcts.net>
2022-05-30 17:59:58 +0100

    DTSPO-8169 - rejig workflow

--- scripts/queue.sh
+++ scripts/queue.sh
@@ -6,15 +6,15 @@ set -e
 # Fill up PR's service bus queue
 
 SERVICE_BUS='sds-keda-stg-01'
-
 QUEUE_NAME="$1"
-QUEUE=$(az servicebus queue create --namespace-name sds-keda-stg-01 \
-        --resource-group sds-keda-stg --name "${QUEUE_NAME}"  \
-        --query name -o tsv)
-
 SB_RESOURCE_GROUP='sds-keda-stg'
 MESSAGES=200
 SCRIPT_DIR=$(dirname "${0}")
+
+# Create queue for pr
+QUEUE=$(az servicebus queue create --namespace-name sds-keda-stg-01 \
+        --resource-group sds-keda-stg --name "${QUEUE_NAME}"  \
+        --query name -o tsv)
 
 cd "${SCRIPT_DIR}" || exit
 ../messageGenerator/recipe-sender "${SERVICE_BUS}.servicebus.windows.net" "${QUEUE}" "${MESSAGES}"
