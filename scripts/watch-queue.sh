7a2c4c4d28e5375298692b0826850a77dc025c18
msl8r <72858053+msl8r@users.noreply.github.com>
2022-05-31 12:12:13 +0100

    DTSPO-8169 - update comments in workflow (#5)
    * DTSPO-8169 - update comments in workflow
    
    * DTSPO-8169 - update comments app code
    
    * DTSPO-8169 - create 500 messages
    
    * DTSPO-8169 - create 500 messages
    
    * DTSPO-8169 - create 1000 messages
    
    * DTSPO-8169 - add sleep to queue watch
    
    * DTSPO-8169 - add concurrency to messageGenerator script
    
    * DTSPO-8169 - test moving vars into workflow file
    
    * DTSPO-8169 - move more vars into workflow file
    
    * DTSPO-8169 - tidy up scripts and workflows
    
    * DTSPO-8169 - add cleanup workflow
    
    * DTSPO-8169 - Put output of message generator in a collapsable group
    
    * DTSPO-8169 - replace recipe-sender binary
    
    * DTSPO-8169 - consolidate go workflow into deployment wf
    
    * DTSPO-8169 - tidy up cleanup.yml
    
    * DTSPO-8169 - tidy up deploy workflow
    
    * DTSPO-8169 - tidy up deploy workflow

--- scripts/watch-queue.sh
+++ scripts/watch-queue.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
-
 set -e
 
 # watch queue until it is empty
-
-SERVICE_BUS='sds-keda-stg-01'
-QUEUE_NAME="$1"
-SB_RESOURCE_GROUP='sds-keda-stg'
-
 until [[ "${CURRENT_QUEUE_SIZE}" == "0" ]]; do
   CURRENT_QUEUE_SIZE=$(az servicebus queue show --resource-group "${SB_RESOURCE_GROUP}" \
       --namespace-name "${SERVICE_BUS}" \
       --name "${QUEUE_NAME}" \
+      --subscription "${SUBSCRIPTION}" \
       --query countDetails.activeMessageCount -o tsv)
   echo "Current queue size: ${CURRENT_QUEUE_SIZE}"
+  sleep 5
 done
 
 echo "${QUEUE_NAME} queue is now empty"
