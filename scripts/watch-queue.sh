cd5d967d598ea949fc747fe55d86eac1fef4d975
msl8r <matt.slater@hmcts.net>
2022-05-31 10:06:18 +0100

    DTSPO-8169 - move more vars into workflow file

--- scripts/watch-queue.sh
+++ scripts/watch-queue.sh
@@ -10,6 +10,7 @@ until [[ "${CURRENT_QUEUE_SIZE}" == "0" ]]; do
   CURRENT_QUEUE_SIZE=$(az servicebus queue show --resource-group "${SB_RESOURCE_GROUP}" \
       --namespace-name "${SERVICE_BUS}" \
       --name "${QUEUE_NAME}" \
+      --subscription "${SUBSCRIPTION}" \
       --query countDetails.activeMessageCount -o tsv)
   echo "Current queue size: ${CURRENT_QUEUE_SIZE}"
   sleep 5
