323254bab85892dcdfc9c9665bf69c2d97a334b5
msl8r <matt.slater@hmcts.net>
2022-06-03 14:40:08 +0100

    DTSPO-8169 - negate queue check

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -21,7 +21,7 @@ az servicebus queue delete \
 # Make sure queue has ben deleted
 count=3
 until [[ $deleted == "true" ]] || [[ $count == 0 ]]; do
-  if [[ $(az servicebus queue show --subscription "${SUBSCRIPTION}" --namespace-name "${SERVICE_BUS}" --resource-group "${SB_RESOURCE_GROUP}" --name "${QUEUE_NAME}") ]]; then
+  if [[ ! $(az servicebus queue show --subscription "${SUBSCRIPTION}" --namespace-name "${SERVICE_BUS}" --resource-group "${SB_RESOURCE_GROUP}" --name "${QUEUE_NAME}") ]]; then
     deleted="true"
     echo "${QUEUE_NAME} queue has been deleted"
   elif [[ $count == 1 ]]; then
