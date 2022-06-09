1437b48e460fb38d1fac57bd873c07fc0536de56
msl8r <matt.slater@hmcts.net>
2022-06-09 13:57:52 +0100

    DTSPO-8169 - fix prod docker build and cleanup

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -11,13 +11,13 @@ fi
 
 if [[ ${BUILD} =~ ^pr-.* ]]; then
 
-  docker build . -t "${ACR_REPO}:${BUILD}"
-  docker push "${ACR_REPO}:${BUILD}"
+  docker build . -t "${ACR_REGISTRY}/${ACR_REPO}:${BUILD}"
+  docker push "${ACR_REGISTRY}/${ACR_REPO}:${BUILD}"
 
 elif [[ ${BUILD} == "prod" ]]; then
   TAG="prod-$(git show --no-patch --no-notes --pretty=format:"%h-%ad" --date=format:'%Y%m%d%H%M%S' "${GITHUB_SHA}")"
-  echo "Promoting ${ACR_REPO}:pr-${GITHUB_EVENT_NUMBER} to ${ACR_REPO}:${TAG}"
-  az acr import --force -n "${REGISTRY_NAME}" --subscription "${REGISTRY_SUB}" --source "${ACR_REPO}:pr-${GITHUB_EVENT_NUMBER}" -t "${TAG}"
+  echo "Promoting ${ACR_REPO}:pr-${GITHUB_EVENT_NUMBER} to ${ACR_REGISTRY}/${ACR_REPO}:${TAG}"
+  az acr import --force -n "${REGISTRY_NAME}" --subscription "${REGISTRY_SUB}" --source "${ACR_REGISTRY}/${ACR_REPO}}:pr-${GITHUB_EVENT_NUMBER}" -t "${ACR_REPO}:${TAG}"
 else
   echo "Build type not recognised, use pr-{pr_number} or prod"
   exit 1
