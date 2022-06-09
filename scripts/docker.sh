d1d949240057f0175dbe376105c61fcb42486722
msl8r <matt.slater@hmcts.net>
2022-06-09 09:49:45 +0100

    DTSPO-8169 - move images to sdshmctspublic

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,7 +3,6 @@ set -e
 
 # PR / prod
 BUILD=$1
-REGISTRY_SUB="8999dec3-0104-4a27-94ee-6588559729d1"
 
 if [[ ${GITHUB_SHA} == "" ]]; then
   echo "No sha found"
@@ -12,13 +11,14 @@ fi
 
 if [[ ${BUILD} =~ ^pr-.* ]]; then
 
-  docker build . -t "${REGISTRY_NAME}.azurecr.io/${APP_NAME}:${BUILD}"
-  docker push "${REGISTRY_NAME}.azurecr.io/${APP_NAME}:${BUILD}"
+  docker build . -t "${ACR_REPO}:${BUILD}"
+  docker push "${ACR_REPO}:${BUILD}"
 
 elif [[ ${BUILD} == "prod" ]]; then
   TAG="prod-$(git show --no-patch --no-notes --pretty=format:"%h-%ad" --date=format:'%Y%m%d%H%M%S' "${GITHUB_SHA}")"
   echo "${TAG}"
-  REPO="${REGISTRY_NAME}.azurecr.io/${APP_NAME}"
-  az acr import --force -n "${REGISTRY_NAME}" --subscription "${REGISTRY_SUB}" --source "${REPO}:pr-${GITHUB_EVENT_NUMBER}" -t "${REPO}:${TAG}"
-
+  az acr import --force -n "${REGISTRY_NAME}" --subscription "${REGISTRY_SUB}" --source "${ACR_REPO}:pr-${GITHUB_EVENT_NUMBER}" -t "${ACR_REPO}:${TAG}"
+else
+  echo "Build type not recognised, use pr-{pr_number} or prod"
+  exit 1
 fi
