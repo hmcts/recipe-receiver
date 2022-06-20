fd85b1665a4af86217651e45162553cd0b775c85
msl8r <matt.slater@hmcts.net>
2022-06-20 16:31:18 +0100

    DTSPO-8223 - watch queue for 5 minutes then fail

--- scripts/watch-queue.sh
+++ scripts/watch-queue.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+
+# Watch the queue for 5 minutes then fail if the queue isn't empty.
+wait_until=$(( $(date +'%H%M%S') + ( 5 * 60 )))
+
 # watch queue until it is empty
 until [[ "${CURRENT_QUEUE_SIZE}" == "0" ]]; do
   CURRENT_QUEUE_SIZE=$(az servicebus queue show --resource-group "${SB_RESOURCE_GROUP}" \
@@ -9,7 +13,13 @@ until [[ "${CURRENT_QUEUE_SIZE}" == "0" ]]; do
       --subscription "${SUBSCRIPTION}" \
       --query countDetails.activeMessageCount -o tsv)
   echo "Current queue size: ${CURRENT_QUEUE_SIZE}"
-  sleep 5
+
+  if [[ $(date +'%H%M%S') > ${wait_until} ]]; then
+    exit 1
+  else
+    sleep 5
+  fi
+
 done
 
 echo "${QUEUE_NAME} queue is now empty"
