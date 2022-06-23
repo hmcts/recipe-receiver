cf5a204af7749fbb19da560990652db8d3710665
msl8r <72858053+msl8r@users.noreply.github.com>
2022-06-23 18:32:48 +0100

    DTSPO-8223 - update workflows to deploy test app to cft on pr (#27)
    * DTSPO-8223 - update workflows to deploy test app to cft on pr
    
    * DTSPO-8223 - update workflows to use acr login to avoid using admin username and password
    
    * DTSPO-8223 - try useing --export token
    
    * DTSPO-8223 - try useing --export token
    
    * DTSPO-8223 - try useing --export token
    
    * DTSPO-8223 - :facepalm:
    
    * DTSPO-8223 - try docker login with sp creds
    
    * DTSPO-8223 - try docker login with sp creds
    
    * DTSPO-8223 - set namespace to deploy to
    
    * DTSPO-8223 - watch queue for 5 minutes then fail
    
    * DTSPO-8223 - fix typo with namespace var
    
    * DTSPO-8223 - fix typo with token var
    
    * DTSPO-8223 - fix promotion
    
    * DTSPO-8223 - Explicitly set subscription
    
    * DTSPO-8223 - remove subscription
    
    * DTSPO-8223 - try setting subscription using az cli
    
    * DTSPO-8223 - try setting subscription using az cli
    
    * DTSPO-8223 - fix cft create queue step
    
    * DTSPO-8223 - fix cft create queue step
    
    * DTSPO-8223 - fix sds subscription
    
    * DTSPO-8223 - fix cft subscription
    
    * DTSPO-8223 - fix cft subscription
    
    * DTSPO-8223 - fix cft build
    
    * DTSPO-8223 - fix cft build
    
    * DTSPO-8223 - fix cft build
    
    * DTSPO-8223 - fix cft namespace
    
    * DTSPO-8223 - move aks auth to bash script
    
    * DTSPO-8223 - move aks auth to bash script
    
    * DTSPO-8223 - move cleanups to separate stages
    
    * DTSPO-8223 - tidy up cleanup workflow
    
    * DTSPO-8223 - improve deploy workflow
    
    * DTSPO-8223 - improve deploy workflow
    
    * DTSPO-8223 - improve deploy workflow
    
    * DTSPO-8223 - improve deploy workflow
    
    * DTSPO-8223 - improve deploy workflow
    
    * DTSPO-8223 - set kubeconfig path
    
    * DTSPO-8223 - set kubeconfig path
    
    * DTSPO-8223 - set kubeconfig path
    
    * DTSPO-8223 - set kubeconfig path
    
    * DTSPO-8223 - remove azure cli step
    
    * DTSPO-8223 - fix time command
    
    * DTSPO-8223 - fix time command
    
    * DTSPO-8223 - update date command to get single digit hour to save octal pain
    
    * DTSPO-8223 - tidy up workflow
    
    * DTSPO-8223 - fix cft cleanup
    
    * DTSPO-8223 - fix cft cleanup
    
    * DTSPO-8223 - rename deploy yaml
    
    * DTSPO-8223 - use bash scripts to deploy to aks
    
    * DTSPO-8223 - use bash scripts to deploy to aks
    
    * DTSPO-8223 - use bash scripts to deploy to aks
    
    * DTSPO-8223 - fix helm sets
    
    * DTSPO-8223 - add helm dependency build
    
    * DTSPO-8223 - add helm dependency build
    
    * DTSPO-8223 - add helm dependency build
    
    * DTSPO-8223 - add helm dependency build
    
    * DTSPO-8223 - add helm dependency build
    
    * DTSPO-8223 - use new method of deployment in sds too
    
    * DTSPO-8223 - use new method of deployment in sds too
    
    * DTSPO-8223 - use new method of deployment in sds too
    
    * DTSPO-8223 - fix infra name
    
    * DTSPO-8223 - don't break pipeline if helm release not found
    
    * DTSPO-8223 - clean up
    
    * DTSPO-8223 - fix timeout on watching queue
    
    * DTSPO-8223 - attempt to reduce amount of workflow code by using a job matrix.
    
    * DTSPO-8223 - fix date command
    
    * DTSPO-8223 - fix image tag and add error message to queue timeout
    
    * DTSPO-8223 - fix image tag and add error message to queue timeout
    
    * DTSPO-8223 - fix image tag and add error message to queue timeout
    
    * DTSPO-8223 - fix path to env var file
    
    * DTSPO-8223 - fix path to env var file
    
    * DTSPO-8223 - fix path to env var file
    
    * DTSPO-8223 - remove comments in vars files
    
    * DTSPO-8223 - fix vars

--- scripts/watch-queue.sh
+++ scripts/watch-queue.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+
+# Watch the queue for 5 minutes then fail if the queue isn't empty.
+#
+QUEUE_TIMEOUT_MINS=5
+wait_until=$(date +'%l%M%S' -d "${QUEUE_TIMEOUT_MINS} mins")
+
 # watch queue until it is empty
 until [[ "${CURRENT_QUEUE_SIZE}" == "0" ]]; do
   CURRENT_QUEUE_SIZE=$(az servicebus queue show --resource-group "${SB_RESOURCE_GROUP}" \
@@ -9,7 +15,16 @@ until [[ "${CURRENT_QUEUE_SIZE}" == "0" ]]; do
       --subscription "${SUBSCRIPTION}" \
       --query countDetails.activeMessageCount -o tsv)
   echo "Current queue size: ${CURRENT_QUEUE_SIZE}"
-  sleep 5
+
+  if [[ $(date +'%l%M%S') > ${wait_until} ]]; then
+    echo "Queue isn't empty after ${QUEUE_TIMEOUT_MINS}, quitting..."
+    exit 1
+  else
+    sleep 5
+  fi
+
 done
 
 echo "${QUEUE_NAME} queue is now empty"
+
+
