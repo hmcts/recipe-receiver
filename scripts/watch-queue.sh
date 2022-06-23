a8ad3b13ae6a55a2392040a5407569fd54fae6a9
msl8r <matt.slater@hmcts.net>
2022-06-23 17:50:34 +0100

    DTSPO-8223 - fix image tag and add error message to queue timeout

--- scripts/watch-queue.sh
+++ scripts/watch-queue.sh
@@ -5,7 +5,7 @@ set -e
 # Watch the queue for 5 minutes then fail if the queue isn't empty.
 #
 QUEUE_TIMEOUT_MINS=5
-wait_until=$(date +'%l%M%S' -d "'${QUEUE_TIMEOUT_MINS} mins'")
+wait_until=$(date +'%l%M%S' -d "${QUEUE_TIMEOUT_MINS} mins")
 
 # watch queue until it is empty
 until [[ "${CURRENT_QUEUE_SIZE}" == "0" ]]; do
