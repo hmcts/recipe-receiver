e67083f7befd19a64043e07e7ca95839d3447ec6
msl8r <matt.slater@hmcts.net>
2022-06-23 10:24:33 +0100

    DTSPO-8223 - update date command to get single digit hour to save octal pain

--- scripts/watch-queue.sh
+++ scripts/watch-queue.sh
@@ -3,7 +3,8 @@ set -e
 
 
 # Watch the queue for 5 minutes then fail if the queue isn't empty.
-wait_until=$(( "$(date +'%H%M%S')" + ( 5 * 60 )))
+#
+wait_until=$(( "$(date +'%l%M%S')" + ( 5 * 60 )))
 
 # watch queue until it is empty
 until [[ "${CURRENT_QUEUE_SIZE}" == "0" ]]; do
@@ -14,7 +15,7 @@ until [[ "${CURRENT_QUEUE_SIZE}" == "0" ]]; do
       --query countDetails.activeMessageCount -o tsv)
   echo "Current queue size: ${CURRENT_QUEUE_SIZE}"
 
-  if [[ $(date +'%H%M%S') > ${wait_until} ]]; then
+  if [[ "$(date +'%l%M%S')" > ${wait_until} ]]; then
     exit 1
   else
     sleep 5
