0fda72deb9c34e5fa52d6555fddcbcfe53d6d05c
msl8r <matt.slater@hmcts.net>
2022-06-24 09:55:51 +0100

    DTSPO-8223 - give cft its own values file

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -19,7 +19,7 @@ if [[ ${ACTION} == "deploy" ]]; then
   helm repo add function https://hmctspublic.azurecr.io/helm/v1/repo
   helm dependency build "${CHART_DIR}"
 
-  helm upgrade --install "${RELEASE_NAME}" "${CHART_DIR}" -n "${KUBE_NAMESPACE}" \
+  helm upgrade -f "${CHART_DIR}/values-${PROJECT}.yaml" --install "${RELEASE_NAME}" "${CHART_DIR}" -n "${KUBE_NAMESPACE}" \
       --set function.image="${ACR_REPO}":pr-"${GITHUB_EVENT_NUMBER}" \
       --set function.environment.QUEUE="${QUEUE_NAME}" \
       --set function.environment.FULLY_QUALIFIED_NAMESPACE="${SERVICE_BUS}.servicebus.windows.net" \
@@ -27,6 +27,7 @@ if [[ ${ACTION} == "deploy" ]]; then
       --set function.triggers[0].namespace="${SERVICE_BUS}" \
       --set function.triggers[0].queueName="${QUEUE_NAME}" \
       --set function.triggers[0].queueLength=5 \
+      --set function.aadIdentityName=
       --wait
 
 elif [[ ${ACTION} == "delete" ]]; then
