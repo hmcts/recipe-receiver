d5f663675db7149b929f7b9cf8052b4d83e35849
msl8r <72858053+msl8r@users.noreply.github.com>
2022-06-30 17:52:15 +0100

    Dtspo 8223 fix deploy (#45)
    * DTSPO-8223 - revert triggers back
    
    * DTSPO-8223 - revert triggers back

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -20,33 +20,31 @@ get_creds() {
 # Try getting Cluster 00 creds first then 01. Fail if problems with both
 get_creds 00 2> "${AKS_LOG_FILE}" || get_creds 01 2> "${AKS_LOG_FILE}" || ( echo "Failed to authenticate after trying both clusters. Errors below." && cat "${AKS_LOG_FILE}"&& exit 1)
 
-
 set -e
 
-for release in ${RELEASES}; do
-  RELEASE_NAME="${APP_NAME}-pr-${release}"
+if [[ ${ACTION} == "deploy" ]]; then
+  RELEASE_NAME="${APP_NAME}-pr-${GITHUB_EVENT_NUMBER}"
+  helm repo add function https://hmctspublic.azurecr.io/helm/v1/repo
+  helm dependency build "${CHART_DIR}"
 
-  if [[ ${ACTION} == "deploy" ]]; then
+  helm upgrade -f "${CHART_DIR}/values-${PROJECT}.yaml" --install "${RELEASE_NAME}" "${CHART_DIR}" -n "${KUBE_NAMESPACE}" \
+      --set function.image="${ACR_REPO}":pr-"${GITHUB_EVENT_NUMBER}" \
+      --set function.environment.QUEUE="${QUEUE_NAME}" \
+      --set function.environment.FULLY_QUALIFIED_NAMESPACE="${SERVICE_BUS}.servicebus.windows.net" \
+      --set function.triggers[0].type=azure-servicebus \
+      --set function.triggers[0].namespace="${SERVICE_BUS}" \
+      --set function.triggers[0].queueName="${QUEUE_NAME}" \
+      --set function.triggers[0].queueLength=5 \
+      --wait
 
-    helm repo add function https://hmctspublic.azurecr.io/helm/v1/repo
-    helm dependency build "${CHART_DIR}"
+elif [[ ${ACTION} == "delete" ]]; then
+  for release in ${RELEASES}; do
+    RELEASE_NAME="${APP_NAME}-pr-${release}"
 
-    helm upgrade -f "${CHART_DIR}/values-${PROJECT}.yaml" --install "${RELEASE_NAME}" "${CHART_DIR}" -n "${KUBE_NAMESPACE}" \
-        --set function.image="${ACR_REPO}":pr-"${GITHUB_EVENT_NUMBER}" \
-        --set function.environment.QUEUE="${QUEUE_NAME}" \
-        --set function.environment.FULLY_QUALIFIED_NAMESPACE="${SERVICE_BUS}.servicebus.windows.net" \
-        --set function.triggers[0].type=azure-servicebus \
-        --set function.triggers[0].namespace="${SERVICE_BUS}" \
-        --set function.triggers[0].queueName="${QUEUE_NAME}" \
-        --set function.triggers[0].queueLength=5 \
-        --wait
-
-  elif [[ ${ACTION} == "delete" ]]; then
-    # Delete helm release if it exists
-    if [[ $(helm list -n "${KUBE_NAMESPACE}" --short --filter "${RELEASE_NAME}" ) != "" ]]; then
-      echo "Deleting release ${RELEASE_NAME}"
-      helm uninstall -n "${KUBE_NAMESPACE}" "${RELEASE_NAME}" --wait
-
-    fi
+  # Delete helm release if it exists
+  if [[ $(helm list -n "${KUBE_NAMESPACE}" --short --filter "${RELEASE_NAME}" ) != "" ]]; then
+    echo "Deleting release ${RELEASE_NAME}"
+    helm uninstall -n "${KUBE_NAMESPACE}" "${RELEASE_NAME}" --wait
   fi
-done
+  done
+fi
