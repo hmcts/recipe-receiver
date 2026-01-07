c0cb0de11396b82036768732126d2c9896a577ab
Remy Thompson <Remy.Thompson@justice.gov.uk>
2026-01-07 11:49:15 +0000

    update version

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -38,7 +38,7 @@ if [[ ${ACTION} == "deploy" ]]; then
   # Deploy from OCI registry
   helm upgrade -f "${CHART_DIR}/values-${PROJECT}.yaml" --install "${RELEASE_NAME}" \
       oci://hmctsprod.azurecr.io/plum/recipe-receiver \
-      --version 1.2.3 \
+      --version pr-"${GITHUB_EVENT_NUMBER}" \
       -n "${KUBE_NAMESPACE}" \
       --set function.image="${ACR_REPO}":pr-"${GITHUB_EVENT_NUMBER}" \
       --set function.environment.QUEUE="${QUEUE_NAME}" \
